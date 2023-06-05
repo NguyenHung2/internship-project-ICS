6ef833ef6cb8aae799ac6a20d5a26fab2df094d7
NguyenHung2 <hn0193348@gmail.com>
2023-06-05 08:25:51 +0700

    05-06-2023 update folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './layouts/component/dashboard/dashboard.component';
-import { ThietbiComponent } from './layouts/component/danhmuc/thietbi/thietbi.component';
-import { NentangComponent } from './layouts/component/danhmuc/nentang/nentang.component';
-import { QuanlygoiComponent } from './layouts/component/quanlygoi/quanlygoi.component';
-import { NangcapthietbiComponent } from './layouts/component/nangcapthietbi/nangcapthietbi.component';
-import { NhatkyComponent } from './layouts/component/nhatky/nhatky.component';
-import { HomeComponent } from './layouts/component/home/home.component';
-import { QuanlysubnetComponent } from './layouts/component/danhmuc/quanlysubnet/quanlysubnet.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { ThietbiComponent } from './modules/danhmuc/thietbi/thietbi.component';
+import { NentangComponent } from './modules/danhmuc/nentang/nentang.component';
+import { QuanlygoiComponent } from './modules/quanlygoi/quanlygoi.component';
+import { NangcapthietbiComponent } from './modules/nangcapthietbi/nangcapthietbi.component';
+import { NhatkyComponent } from './modules/nhatky/nhatky.component';
+import { HomeComponent } from './modules/home/home.component';
+import { QuanlysubnetComponent } from './modules/danhmuc/quanlysubnet/quanlysubnet.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent,},
